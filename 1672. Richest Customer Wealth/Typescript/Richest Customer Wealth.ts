0f2abea16fce88263a703424a0c9c97a33ecdcf4
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-08-12 23:54:00 +0200

    Solve richest-customer-wealth problem

---
+++ 1672. Richest Customer Wealth/Typescript/Richest Customer Wealth.ts
@@ -0,0 +1,41 @@
+/*
+Problem Link: {https://leetcode.com/problems/richest-customer-wealth/submissions/}
+*/
+
+// Solution 1
+// function maximumWealth(accounts: number[][]): number {
+//   let maxArr = []
+
+//   maxArr = accounts.map(acc => acc.reduce((init, curr) => (init + curr)))
+
+//   return Math.max(...maxArr)
+// };
+
+// Solution 2
+function maximumWealth(accounts: number[][]): number {
+  let maxValue = 0;
+
+  accounts.forEach((acc) => {
+    const sum = acc.reduce((init, curr) => init + curr);
+
+    maxValue = sum > maxValue ? sum : maxValue;
+  });
+
+  return maxValue;
+}
+
+// TESTING HERE
+console.log(
+  maximumWealth([
+    [1, 2, 3],
+    [3, 2, 1],
+  ])
+);
+
+console.log(
+  maximumWealth([
+    [1, 5],
+    [7, 3],
+    [3, 5],
+  ])
+);
